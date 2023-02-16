978a0ec6a19554696701ae55bae950b6626404d6
shoot16625 <shuto.uchida@gunosy.com>
2023-02-16 21:31:19 +0900

    add app source

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,22 +1,13 @@
-import { createApp } from "solid-utils";
-import { createSignal, onCleanup } from "solid-js";
-import { MetaProvider } from "solid-meta";
-import "./css/index.css";
-
-// 1秒ごとにカウントするコンポーネント
-const CountingComponent = () => {
-  const [count, setCount] = createSignal(0);
-  const interval = setInterval(() => setCount((count) => count + 1), 1000);
-  onCleanup(() => clearInterval(interval));
-  return <div class="text-red-500">Count value is {count()}</div>;
-};
+import { render } from "solid-js/web";
+import TopPage from "./components/top_page";
+import "./css/input.css";
 
 const App = () => {
   return (
     <>
-      <CountingComponent />
+      <TopPage />
     </>
   );
 };
 
-createApp(App).use(MetaProvider).mount("#root");
+render(() => <App />, document.getElementById("root"));
